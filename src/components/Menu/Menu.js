273e8eccc1032308e134a934030b9caa37ca7fbc
rumeysaka <73015083+rumeysaka@users.noreply.github.com>
2022-01-25 21:17:57 +0300

    en-baş

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./Menun.css"
+
+export default function Menu() {
+  return
+  <>
+  <div classNameName="page">
+  <nav classNameName="page__menu menu">
+    <ul classNameName="menu__list r-list">
+      <li classNameName="menu__group"><a href="#0" className="menu__link r-link text-underlined">Option #1</a></li>
+      <li className="menu__group"><a href="#0" className="menu__link r-link text-underlined">Option #2</a></li>
+      <li className="menu__group"><a href="#0" className="menu__link r-link text-underlined">Option #3</a></li>
+    </ul>
+  </nav>
+  <nav className="page__menu page__custom-settings menu">
+    <ul className="menu__list r-list">
+      <li className="menu__group"><a href="#0" className="menu__link r-link text-underlined">Option #1</a></li>
+      <li className="menu__group"><a href="#0" className="menu__link r-link text-underlined">Option #2</a></li>
+      <li className="menu__group"><a href="#0" className="menu__link r-link text-underlined">Option #3</a></li>
+    </ul>
+  </nav>
+</div>
+</>
+}
